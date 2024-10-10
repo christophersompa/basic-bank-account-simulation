0fb0e9aed4307670a5e66f7d7049e3e913bff507
Christopher <cgtsompa@gmail.com>
2024-10-10 21:49:47 +0100

    Add project files.

---
+++ basic-bank-accont-simulation/Class/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿namespace basic_bank_accont_simulation.Class
+{
+    internal class BankAccount
+    {
+        private decimal balance;
+
+        private string Name { get; set; }
+
+        public BankAccount(decimal initalBalance)
+        {
+            balance = initalBalance; 
+        }
+
+        public decimal Deposit(decimal amount)
+        {
+            return balance += amount;
+        }
+
+        public decimal Withdraw(decimal amount)
+        {
+            return balance -= amount;  
+        }
+
+        public decimal GetBalance(decimal startingBalance) 
+        { 
+            return balance; 
+        }
+    }
+}
---
+++ basic-bank-accont-simulation/Program.cs
@@ -0,0 +1,100 @@
+﻿using basic_bank_accont_simulation.Class;
+using System;
+
+namespace basic_bank_accont_simulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to our bank\n");
+
+            Console.WriteLine("Please enter your name:");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("\nEnter your starting balance:");
+
+            decimal startingBalance = Convert.ToDecimal(Console.ReadLine());
+
+            BankAccount account = new BankAccount(startingBalance);
+
+            Console.WriteLine($"\nAccount created! Welcome to you new bank account {name}!");
+
+            bool isRunning = true;
+
+            while(isRunning == true)
+            {
+                Console.WriteLine("\nActions: 1. Balance, 2. Deposit , 3. Withdraw , 4. Exit");
+
+                string action = Console.ReadLine();
+
+                decimal balance = account.GetBalance(startingBalance);
+
+                if (action != "balance"  &&  action != "1" && 
+                    action != "deposit"  &&  action != "2" && 
+                    action != "withdraw" &&  action != "3" && 
+                    action != "exit"     &&  action != "4")
+                {
+                    Console.WriteLine("\nIncorrect action! Please try again!");
+                }
+                else if (action == "balance" || action == "1")
+                {
+                    Console.WriteLine($"\nYour balance is ${balance}");
+                }
+                else if (action == "deposit" || action == "2")
+                {
+                    Console.WriteLine("\nEnter the amount you'd like to deposit:");
+
+                    decimal depositAmount = Convert.ToDecimal(Console.ReadLine());
+
+                    decimal newBalance = account.Deposit(depositAmount);
+
+                    Console.WriteLine($"\nYour new balance is: ${newBalance}");
+                }
+                else if (action == "withdraw" || action == "3")
+                {
+                    Console.WriteLine("\nEnter the amount you'd like to withdraw:");
+
+                    decimal withdrawAmount = Convert.ToDecimal(Console.ReadLine());
+
+                    if (withdrawAmount > balance)
+                    {
+                        Console.WriteLine($"\nInsufficent fund you available balance is: ${balance}");
+                    } 
+                    else
+                    {
+                        decimal newBalance = account.Withdraw(withdrawAmount);
+
+                        Console.WriteLine($"\nYour new balance is: ${newBalance}");
+                    }
+                }
+                else if (action == "exit"|| action == "4")
+                {
+                    Console.WriteLine("\nAre you sure you would like to exit your account?");
+
+                    Console.WriteLine("\nY : N ?");
+
+                    string isExiting = Console.ReadLine();
+
+                    if (isExiting == "y" || isExiting == "Y")
+                    {
+                        isRunning = false;
+
+                        Console.WriteLine("\nThank you for banking with us!");
+                    } 
+                    else if (isExiting == "n" || isExiting == "N")
+                    {
+                        isRunning = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect input!");
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
